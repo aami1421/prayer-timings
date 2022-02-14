b302b65c2f4e605344d5f2e077f5970240baf5bf
aami1421 <aqeelalmosa14@gmail.com>
2022-02-14 15:42:13 +0300

    Update script.js
    resolved city issue

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 
-
+function getCity(){}
 $.ajax({
   url: "https://geolocation-db.com/jsonp",
   jsonpCallback: "callback",
   dataType: "jsonp",
   success: function(location) {
     console.log(location);
-    $("#city").html(location.city);
     var city= location.city;
     var country= location.country_name;
+    var latitude = location.latitude;
+    var longitude = location.longitude;
+    var IPv4 = location.IPv4;
+    let Url;
+    if(city === null){
+      Url="http://api.aladhan.com/v1/timingsByAddress?address="+latitude+","+longitude
+      $.ajax({
+        url: "https://geolocation-db.com/jsonp/"+IPv4,
+        jsonpCallback: "callback",
+        dataType: "jsonp",
+        success: function(info) {
+          city= info.city;
+          $("#city").html(info.city);
 
+            }
+          });
+    }else{
+      $("#city").html(location.city);
+      Url="https://api.aladhan.com/v1/timingsByCity?city="+city+"&country="+country+"&method=8"
+      }
     $.getJSON(
-      "https://api.aladhan.com/v1/timingsByCity?city="+city+"&country="+country+"&method=8",
-     // "http://api.aladhan.com/v1/timingsByCity?"+city+"=Dhahran&"+country+"=saudi%20arabia&method=8",
+      Url,
+
        function (data) {
          console.log(data);
+        // data.data.meta.latitude = latitude;
+        // data.data.meta.longitude= longitude;
          let date= data.data.date.hijri.date;
          let fajr= data.data.timings.Fajr;
          let sunrise= data.data.timings.Sunrise;
